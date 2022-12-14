b97cf2350cd9befd260aaf186699353c9ce42737
Christopher Ribeiro <christo_campiglia@hotmail.com>
2022-12-14 18:20:44 -0300

    cleanup

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import "./style.css"
+import { spawn, alive } from "../lib"
+
+let id = spawn((max) => {
+    const primeNumbers = []
+    const isPrime = (num) => {
+        for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+            if (num % i === 0) return false
+        return num > 1
+    }
+    let i = 0
+    while (i < max) {
+        if (isPrime(i)) {
+            primeNumbers.push(i)
+        }
+        i++
+    }
+    return primeNumbers
+}, 10000)
+
+id = spawn(() => 1 + 2)
+spawn(() => { })
+id = spawn(() => me() === 1)
+
+console.log(alive(id))
