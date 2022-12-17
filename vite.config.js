1e19530664162d9fd74295d4fe7cb83e6aa61d00
Christopher Ribeiro <christo_campiglia@hotmail.com>
2022-12-17 18:24:02 -0300

    setup build options

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,22 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-    root: './src'
+    root: './src',
+    build: {
+        emptyOutDir: false,
+        outDir: resolve(__dirname, 'dist'),
+        sourcemap: true,
+        lib: {
+            entry: resolve(__dirname, 'lib/index.js'),
+            name: 'Sever',
+            formats: ['iife', 'es'],
+            fileName: (format) => {
+                if (format === 'es') {
+                    return 'sever.js'
+                }
+                return `sever.${format}.js`
+            },
+        },
+    },
 })
