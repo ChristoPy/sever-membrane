3b0febde4a04febb14c39d546ec1860e9bc89665
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-02-17 12:15:48 -0300

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         sourcemap: true,
         lib: {
             entry: resolve(__dirname, 'lib/index.js'),
-            name: 'Sever Membrane',
+            name: 'Sever',
             formats: ['iife', 'es'],
             fileName: (format) => {
                 if (format === 'es') {
